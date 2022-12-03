f6504c5da2f7c06319f157f5f6abbf7230c7c9cc
CaMi <jeanpaulcami@live.com>
2022-12-03 14:29:58 +0200

    dash touches

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,23 +4,139 @@
     <li class="breadcrumb-item text-dark">Dashboard</li>
 @endsection
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header align-items-center">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
         </div>
+    @endif
+    <div class="row g-5 g-xl-10">
+
+        <div class="col-sm-6 col-xl-4 mb-xl-10">
+            <!--begin::Card widget 2-->
+            <div class="card h-lg-100">
+                <!--begin::Body-->
+                <div class="card-body d-flex justify-content-center align-items-start flex-column">
+                    <!--begin::Icon-->
+                    <div class="m-0">
+                        <!--begin::Svg Icon | path: icons/duotune/maps/map004.svg-->
+                        <span class="svg-icon svg-icon-2hx svg-icon-gray-600">
+													<svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                         class="w-6 h-6">
+                                  <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"/>
+                                </svg>
+
+                        </span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Icon-->
+                    <!--begin::Section-->
+                    <div class="d-flex flex-column my-7">
+                        <!--begin::Number-->
+                        <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">
+                            {{ \App\Models\Project::count() }}
+                        </span>
+                        <!--end::Number-->
+                        <!--begin::Follower-->
+                        <div class="m-0">
+                            <span class="fw-semibold fs-6 text-gray-400">Projects</span>
+                        </div>
+                        <!--end::Follower-->
+                    </div>
+                    <!--end::Section-->
+
+                </div>
+                <!--end::Body-->
+            </div>
+            <!--end::Card widget 2-->
+        </div>
+
+        <div class="col-sm-6 col-xl-4 mb-xl-10">
+            <!--begin::Card widget 2-->
+            <div class="card h-lg-100">
+                <!--begin::Body-->
+                <div class="card-body d-flex justify-content-center align-items-start flex-column">
+                    <!--begin::Icon-->
+                    <div class="m-0">
+                        <!--begin::Svg Icon | path: icons/duotune/graphs/gra001.svg-->
+                        <span class="svg-icon svg-icon-2hx svg-icon-gray-600">
+													<svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                         class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round"
+        d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"/>
+</svg>
+
+												</span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Icon-->
+                    <!--begin::Section-->
+                    <div class="d-flex flex-column my-7">
+                        <!--begin::Number-->
+                        <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">
+                            {{ \App\Models\Team::count() }}
+                        </span>
+                        <!--end::Number-->
+                        <!--begin::Follower-->
+                        <div class="m-0">
+                            <span class="fw-semibold fs-6 text-gray-400">
+                                Team members
+                            </span>
+                        </div>
+                        <!--end::Follower-->
+                    </div>
+                    <!--end::Section-->
+
+                </div>
+                <!--end::Body-->
+            </div>
+            <!--end::Card widget 2-->
+        </div>
+        <div class="col-sm-6 col-xl-4 mb-xl-10">
+            <!--begin::Card widget 2-->
+            <div class="card h-lg-100">
+                <!--begin::Body-->
+                <div class="card-body d-flex justify-content-center align-items-start flex-column">
+                    <!--begin::Icon-->
+                    <div class="m-0">
+                        <!--begin::Svg Icon | path: icons/duotune/graphs/gra001.svg-->
+                        <span class="svg-icon svg-icon-2hx svg-icon-gray-600">
+													<svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                         class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round"
+        d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"/>
+</svg>
+
+
+												</span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Icon-->
+                    <!--begin::Section-->
+                    <div class="d-flex flex-column my-7">
+                        <!--begin::Number-->
+                        <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">
+                            {{ \App\Models\Event::count() }}
+                        </span>
+                        <!--end::Number-->
+                        <!--begin::Follower-->
+                        <div class="m-0">
+                            <span class="fw-semibold fs-6 text-gray-400">
+                              Events
+                            </span>
+                        </div>
+                        <!--end::Follower-->
+                    </div>
+                    <!--end::Section-->
+
+                </div>
+                <!--end::Body-->
+            </div>
+            <!--end::Card widget 2-->
+        </div>
+
     </div>
 @endsection
